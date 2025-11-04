833535bf194136992a189e66ce4e49fa1cdf4c11
illinai <144272615+illinai@users.noreply.github.com>
2025-11-04 02:22:57 -0800

    nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <div className="nav-container">
+                <div className="nav-logo">
+                    <h2>Illina</h2>
+                </div>
+
+                <ul className="nav-menu">
+                    <li><a href="#hero">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
