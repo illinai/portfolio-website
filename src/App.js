989ac1d8b703f64ba11dd27010df239fb240c043
illinai <144272615+illinai@users.noreply.github.com>
2025-11-18 17:57:11 -0500

    app update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +48,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
